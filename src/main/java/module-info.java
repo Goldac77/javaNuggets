01ccd7dd7e51787bdec9ad9b5d2dd7b75878c524
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-07-07 22:47:52 +0000

    initialised project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.javanuggets {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+            
+                            
+    opens com.example.javanuggets to javafx.fxml;
+    exports com.example.javanuggets;
+}
